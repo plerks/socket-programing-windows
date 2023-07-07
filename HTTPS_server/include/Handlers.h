72c8ee91e63ff7f967f434695b0babf9551062f2
plerks <plerks@163.com>
2023-07-07 09:22:08 +0800

    add description about coping with the receiving of request body

--- HTTPS_server/include/Handlers.h
+++ HTTPS_server/include/Handlers.h
@@ -10,7 +10,11 @@
 
 typedef unsigned char byte;
 
-// The Request implementation does't contain body here, the best implementation might be wrapping the body as an Inputstream
+/* The Request implementation does't contain body here. The headers are supposed to be fully received in advance by detecting the emptyLine.
+About the body, the best implementation might be like:
+1. The request has specified a specific Content-Length header. Then just receive the request in a buffer with pre-configured max-request-size.
+2. The request has no specific Content-Length header (for example, with `Transfer-Encoding: chunked` header). Then wrap the body as an InputStream.
+*/
 struct Request {
     SOCKET sock;
     char *requestLine;
